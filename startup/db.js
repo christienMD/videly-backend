27e0def9d7f18d0cb71bba2860b87c9f6ee0e300
Md Christien <mdchristien12@gmail.com>
2023-10-19 09:46:26 +0100

    modified the db  connection string to access render

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,6 @@ const config = require("config");
 
 module.exports = function () {
   // const db = config.get("db");
-  const db = config.get("db");
+  const db = process.env.MONGODB_URI || config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
