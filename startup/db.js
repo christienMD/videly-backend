662c26321c06213d1a24afc9e48f2e180080f3a1
Md Christien <mdchristien12@gmail.com>
2023-10-19 07:51:07 +0100

    added support for reading connection string from an environment variable

--- startup/db.js
+++ startup/db.js
@@ -6,6 +6,7 @@ const config = require('config');
 
 module.exports = function() {
   // const db = config.get('db');
+  console.log("All environment variables:", process.env);
   const db = process.env.MONGODB_URI;
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
