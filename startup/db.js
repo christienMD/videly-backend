3325b3bbb0a7326d044f8bbb5c70a071710eb421
Md Christien <mdchristien12@gmail.com>
2023-10-19 08:18:47 +0100

    modified mongoose connection

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,8 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-
-
-module.exports = function() {
-  // const db = config.get('db');
-  console.log("All environment variables:", process.env);
-  const db = process.env.MONGODB_URI;
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  const db = config.get("db");
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+};
