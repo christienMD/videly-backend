fe1c843c161da2cc23a89769ff2c6790824c30f5
Md Christien <mdchristien12@gmail.com>
2023-10-23 21:08:21 +0100

    refactore seed.js

--- seed.js
+++ seed.js
@@ -40,7 +40,7 @@ const data = [
 
 async function seed() {
   const db =
-    "mongodb+srv://vidleyUser:Lionofjud%40h@nodevidelycluster.cwo0gic.mongodb.net/videly";
+    "mongodb+srv://vidleyUser:Lionofjud%40h@nodevidelycluster.cwo0gic.mongodb.net/";
 
   // console.log("db seed: ", db);
   await mongoose.connect(db);
